dcb25bf9ae5b25b0d72bd9f6a971b8f84982e586
jkhvacmd209 <joppe.koops@hva.nl>
2024-02-08 15:43:38 +0100

    Add beginning of frontend routing

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,3 +1,76 @@
+import about from "./views/about.js";
+
+const pathToRegex = (path) => {
+	return new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+}
+
+const getParams = (match) => {
+	const values = match.result.slice(1);
+	const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+	console.log(Array.from(match.route.path.matchAll(/:(\w+)/g)))
+}
+
+const navigateTo = (url) => {
+	history.pushState(null, null, url);
+	router();
+}
+
+const router = async () => {
+
+
+	const routes = [{
+			path: '/',
+			view: about
+		},
+		{
+			path: '/visited-countries/:id',
+			view: about
+		}
+		// {
+		// 	path: '/visited-countries',
+		// 	view: () => console.log("viewing visited")
+		// },
+		// {
+		// 	path: '/bucketlist-countries',
+		// 	view: () => console.log("viewing bucketlist")
+		// }
+	];
+
+	const potentialMatches = routes.map(route => {
+		return {
+			route: route,
+			result: location.pathname.match(pathToRegex(route.path))
+		}
+	});
+
+	let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+
+	if (!match) {
+		match = {
+			route: routes[0],
+			isMatch: true
+		}
+	}
+
+	const view = new match.route.view(getParams(match));
+
+	document.querySelector('main').innerHTML = await view.getHtml();
+
+}
+
+window.addEventListener('popstate', router)
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.addEventListener('click', e => {
+		if (e.target.matches("[data-link]")) {
+			e.preventDefault();
+			navigateTo(e.target.href);
+		}
+	})
+	router()
+})
+
 const getInfo = () => {
 	const request = new Request("../data.json");
 
