99613f59e6d55f576e103e05e694cb1e959855b4
jkhvacmd209 <joppe.koops@hva.nl>
2024-02-12 14:35:27 +0100

    enable Cors for info.json

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 //app.use(express.static(path.resolve(__dirname, 'docs', 'static')))
 
-app.use('/images', express.static(path.resolve(__dirname, 'docs', 'images')))
-app.use('/scripts', express.static(path.resolve(__dirname, 'docs', 'scripts')))
-app.use('/styles', express.static(path.resolve(__dirname, 'docs', 'styles')))
-app.use('/info.json', express.static(path.resolve(__dirname, 'docs', 'info.json')))
+app.use('/images', express.static(path.resolve(__dirname, 'docs', 'images')));
+app.use('/scripts', express.static(path.resolve(__dirname, 'docs', 'scripts')));
+app.use('/styles', express.static(path.resolve(__dirname, 'docs', 'styles')));
+
+app.get("/info.json", cors(), (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'docs', 'info.json'));
+})
 
 app.get("/*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'docs', 'index.html'));
